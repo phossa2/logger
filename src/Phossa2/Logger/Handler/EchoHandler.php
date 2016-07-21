b8143ab4b4b458ed2caf639244cde78d601a6b0b
Hong Zhang <phossa@126.com>
2016-07-21 13:41:34 +0800

    added EchoHandler

---
+++ src/Phossa2/Logger/Handler/EchoHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Phossa Project
+ *
+ * PHP version 5.4
+ *
+ * @category  Library
+ * @package   Phossa2\Logger
+ * @copyright Copyright (c) 2016 phossa.com
+ * @license   http://mit-license.org/ MIT License
+ * @link      http://www.phossa.com/
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa2\Logger\Handler;
+
+use Phossa2\Logger\Entry\LogEntryInterface;
+
+/**
+ * EchoHandler
+ *
+ * Directly echo the log message
+ *
+ * @package Phossa2\Logger
+ * @author  Hong Zhang <phossa@126.com>
+ * @see     HandlerAbstract
+ * @version 2.0.0
+ * @since   2.0.0 added
+ */
+class EchoHandler extends HandlerAbstract
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function write(LogEntryInterface $logEntry)
+    {
+        echo $logEntry->getFormatted() . $this->getEol();
+    }
+}
